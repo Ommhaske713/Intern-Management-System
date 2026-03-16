aa0f0c8e2c0a3f1b7e8bc33b8e963be1f4748c38
om mhaske <ommhaske1224@gmail.com>
2026-03-16 18:38:23 +0530

    Added GET endpoint to get certificates

--- app/api/certificates/route.ts
+++ app/api/certificates/route.ts
@@ -1,6 +1,10 @@
 import { NextRequest } from 'next/server';
 import { certificateController } from '@/server/controllers/certificate.controller';
 
+export async function GET(req: NextRequest) {
+  return await certificateController.getCertificate(req);
+}
+
 export async function POST(req: NextRequest) {
   const result = await certificateController.issueCertificate(req);
   return result;
